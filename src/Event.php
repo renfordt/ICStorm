a3d5d5fee209bfa05d7811368f41d5f537190ade
Jannik Renfordt <jannik.renfordt@ikobb.de>
2024-03-27 21:18:54 +0100

    Add Event class with getter and setter methods
    A new Event class has been added into the codebase. This class includes private properties for various event attributes, as well as their corresponding getter and setter methods. This will be useful in managing event details.

---
+++ src/Event.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace renfordt\ICStorm;
+
+class Event
+{
+    private string $description;
+    private string $location;
+    private string $startDate;
+    private string $endDate;
+    private string $title;
+    private string $summary;
+    private EventClass $class = EventClass::Private;
+    private EventTransparency $transparency = EventTransparency::Opaque;
+
+
+    private string $conference;
+
+
+    public function setDetails(array $details): void
+    {
+
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getLocation(): string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): void
+    {
+        $this->location = $location;
+    }
+
+    public function getStartDate(): string
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(string $startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    public function getEndDate(): string
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(string $endDate): void
+    {
+        $this->endDate = $endDate;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getSummary(): string
+    {
+        return $this->summary;
+    }
+
+    public function setSummary(string $summary): void
+    {
+        $this->summary = $summary;
+    }
+
+    public function getClass(): EventClass
+    {
+        return $this->class;
+    }
+
+    public function setClass(EventClass $class): void
+    {
+        $this->class = $class;
+    }
+
+    public function getTransparency(): EventTransparency
+    {
+        return $this->transparency;
+    }
+
+    public function setTransparency(EventTransparency $transparency): void
+    {
+        $this->transparency = $transparency;
+    }
+
+
+}
